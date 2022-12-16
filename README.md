aa17f8cb1a6391520d4f0468b97bf924415d9500
younhwan97 <younhwan0903@gmail.com>
2022-12-16 16:16:47 +0900

    [Impl] ImagePreview composable

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # how-to-create-android-library
+
+## 1. 라이브러리 제공을 위한 프로젝트 생성
+
+`Android Studio → New Project → Empty Compose Activity`
+
+간단한 이미지 미리보기 라이브러리를 위해 다음과 같이 코드를 작성한다.
+
+```Kotlin
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HowToCreateAndroidLibraryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Box() {
+                        ImagePreview(
+                            image = painterResource(id = R.drawable.sample),
+                            description = "Hello",
+                            modifier = Modifier
+                                .size(150.dp)
+                                .align(Alignment.Center)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImagePreview(
+    image: Painter,
+    modifier: Modifier = Modifier,
+    description: String = "",
+    contentDescription: String = "",
+    onImageClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .shadow(5.dp, RoundedCornerShape(5.dp))
+            .clickable { onImageClick() }
+    ) {
+        Image(
+            painter = image,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.body1,
+            color = Color.White,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(
+                            Color.Transparent,
+                            Color.Black
+                        )
+                    )
+                )
+                .align(Alignment.BottomStart)
+                .padding(8.dp)
+        )
+    }
+}
+```
+
+<br>
+
+## 2. 
--- app/src/main/java/kr/co/younhwan/howtocreateandroidlibrary/MainActivity.kt
+++ app/src/main/java/kr/co/younhwan/howtocreateandroidlibrary/MainActivity.kt
@@ -3,13 +3,26 @@ package kr.co.younhwan.howtocreateandroidlibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import kr.co.younhwan.howtocreateandroidlibrary.ui.theme.HowToCreateAndroidLibraryTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +32,15 @@ class MainActivity : ComponentActivity() {
             HowToCreateAndroidLibraryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Box() {
+                        ImagePreview(
+                            image = painterResource(id = R.drawable.sample),
+                            description = "Hello",
+                            modifier = Modifier
+                                .size(150.dp)
+                                .align(Alignment.Center)
+                        )
+                    }
                 }
             }
         }
@@ -27,14 +48,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HowToCreateAndroidLibraryTheme {
-        Greeting("Android")
+fun ImagePreview(
+    image: Painter,
+    modifier: Modifier = Modifier,
+    description: String = "",
+    contentDescription: String = "",
+    onImageClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .shadow(5.dp, RoundedCornerShape(5.dp))
+            .clickable { onImageClick() }
+    ) {
+        Image(
+            painter = image,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.body1,
+            color = Color.White,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(
+                            Color.Transparent,
+                            Color.Black
+                        )
+                    )
+                )
+                .align(Alignment.BottomStart)
+                .padding(8.dp)
+        )
     }
 }
