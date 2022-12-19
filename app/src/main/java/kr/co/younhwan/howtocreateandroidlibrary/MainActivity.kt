928075fa5bfd4348277c33140fc9f3576e2e79a0
younhwan97 <younhwan0903@gmail.com>
2022-12-19 16:57:07 +0900

    [Refactor] MainActivity

--- app/src/main/java/kr/co/younhwan/howtocreateandroidlibrary/MainActivity.kt
+++ app/src/main/java/kr/co/younhwan/howtocreateandroidlibrary/MainActivity.kt
@@ -3,20 +3,13 @@ package kr.co.younhwan.howtocreateandroidlibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import kr.co.younhwan.howtocreateandroidlibrary.ui.theme.HowToCreateAndroidLibraryTheme
 import kr.co.younhwan.image_preview.ImagePreview
-import kr.co.younhwan.simpleloading.SimpleLoading
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,12 +24,6 @@ class MainActivity : ComponentActivity() {
                             description = "",
                             contentDescription = "",
                             onImageClick = {}
-                        )
-
-                        val state = remember { mutableStateOf(true) }
-
-                        SimpleLoading(
-                            openDialogCustom = state
                         )
                     }
                 }
